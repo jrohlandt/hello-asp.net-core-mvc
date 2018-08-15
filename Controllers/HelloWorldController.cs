46dc8c583f24d37cb24bf635c3ab8c503c986116
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-08-15 12:20:04 +0200

    First controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is index method.";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID {ID}.");
+        }
+    }
+}
